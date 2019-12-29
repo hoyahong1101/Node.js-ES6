01dae0fb137978cbec2063d759ab9abdd8a17ab4
hoyahong1101 <maxhsm1101>
2019-12-29 17:02:23 +0900

    summary

---
+++ 03.test/app2.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const host = '127.0.0.1';
+const port = 3000;
+const bodyParser = require("body-parser"); //Middleware
+
+// 지정한 public 폴더를 client가 접근 가능한 정적 폴더로 만든다.
+
+app.locals.pretty = true;
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false})); 
+app.use("/", express.static("./public"));
+
+app.listen(port, () => {
+	console.log(`http://${host}:${port}`);
+});
+
+app.get("/", (req,res) =>{
+    res.send("<h1>Hello World~~~~~</h1>");
+});
+
+/* app.get("/hello", (req,res) =>{
+    res.send("<h1>Hello World2~</h1>");
+}); */
+
+app.get("/home", (req, res) =>{
+    let name = req.query.name;
+    res.send(`<h1>Mr.${name} Nice to meet you hi hihi.</h1>`);
+});
+
+app.get("/api/user", (req, res)=>{
+    let users = {
+        users : [
+            {id: 1, name: "홍길동", age: 25},
+            {id: 2, name: "홍길만", age: 32},
+            {id: 3, name: "홍길룡", age: 28},
+        ],
+        cnt: 3
+    };
+    res.json(users);
+    
+});
+
+
+
+app.get("/blog/:category/:id", (req,res) => {
+    let category = req.params.category;
+    let id = req.params.id;
+    res.send(`category : ${category}, id: ${id}`);
+ 
+});
+
+app.get("/home2", (req, res)=>{
+    res.sendFile("./public/index.html");
+});
+
+app.post("/join", (req, res)=>{
+    let userid = req.body.userid;
+    let userpw = req.body.userpw;
+    res.send(`userid: ${userid} / userpw ${userpw}`);
+});
+
+app.get("/pug", (req,res)=>{
+    let vals = {
+        title : "PUG 연습",
+        name : req.query.name || 'TEST',
+    }
+    res.render("form.pug", vals);
+});
